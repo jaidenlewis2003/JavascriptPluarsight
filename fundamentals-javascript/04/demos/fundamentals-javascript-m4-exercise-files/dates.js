91873c9282dcb15d8f499c79066c2cabac164cdd
Jaiden Lewis <jaiden0706@gmail.com>
2024-04-24 13:18:05 -0400

    math & comparison v.1

--- fundamentals-javascript/04/demos/fundamentals-javascript-m4-exercise-files/dates.js
+++ fundamentals-javascript/04/demos/fundamentals-javascript-m4-exercise-files/dates.js
@@ -7,21 +7,23 @@ console.log(`Calendar Date: ${date1.toDateString()}`);
 
 // Display a locale-specfic date string 
 console.log(`Locale en-US: ${date1.toLocaleDateString('en-US')}`);
+console.log(`Locale en-GB: ${date1.toLocaleDateString('en-GB')}`);
+console.log(`Locale ja-JP: ${date1.toLocaleDateString('ja-JP')}`);
 
 
+// Display just the time 
+console.log(`Date Time: ${date1.toTimeString()}`);
 
+// Display a locale-specific time string 
+console.log(`Locale en-US: ${date1.toLocaleDateString('en-US')}`);
 
-
-
-
-
-
-
-
-
-
-
-
+// Custom date string 
+let options = {
+    dateStyle: "short",
+    timeStyle: "short" 
+};
+console.log(`Custom Date: ${date1.toLocaleString('en-US', options)}`);
+console.log(`Custom Date: ${date1.toLocaleString('ar-Kw', options)}`);
 
 
 
